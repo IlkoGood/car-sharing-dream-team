515dadf4a25b3e79e1f752cf6ac5cd7f7905176d
Oleksandr Krasnov <redman2775@gmail.com>
2023-05-30 16:02:31 +0300

    created init file for DB

--- src/main/resources/db/changelog/changes/init_db.sql
+++ src/main/resources/db/changelog/changes/init_db.sql
@@ -3,43 +3,43 @@ USE `car_sharing_db`;
 
 CREATE TABLE IF NOT EXISTS `cars`
 (
-    `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
     `model` VARCHAR(255) NOT NULL,
     `brand` VARCHAR(255) NOT NULL,
     `type` ENUM('SEDAN', 'SUV', 'HATCHBACK', 'UNIVERSAL') NOT NULL,
-    `inventory` BIGINT(11),
+    `inventory` BIGINT,
     `daily_fee` DECIMAL NOT NULL,
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `users`
 (
-    `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL,
     `first_name` VARCHAR(255),
     `last_name` VARCHAR(255),
     `password` VARCHAR(255) NOT NULL,
     `role` ENUM('MANAGER', 'COSTUMER') NOT NULL,
-    CONSTRAINT users_pk PRIMARY KEY (`id`)
+    CONSTRAINT users_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS `rentals`
 (
-    `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
     `rental_date` DATE NOT NULL,
     `return_date` DATE NOT NULL,
-    `actual_return_date` DATE NOT NULL,
-    `car_id`,
-    `user_id`,
+    `actual_return_date` DATE DEFAULT NULL,
+    `car_id` BIGINT DEFAULT NULL,
+    `user_id` BIGINT DEFAULT NULL,
     );
 
 CREATE TABLE IF NOT EXISTS `payments`
 (
-    `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
     `status` ENUM('PENDING', 'PAID') NOT NULL,
     `type` ENUM('PAYMENT', 'FINE') NOT NULL,
-    `rental_id` ,
-    `session_url` ,
-    `session_id` ,
+    `rental_id` BIGINT NOT NULL,
+    `session_url` VARCHAR(255),
+    `session_id` BIGINT NOT NULL,
     `amount` DECIMAL NOT NULL
     );
