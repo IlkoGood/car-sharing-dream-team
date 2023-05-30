db48df2872ab3ce2da478bc7ad452f49e7c5a474
Illia Kovtun <116804521+IlkoGood@users.noreply.github.com>
2023-05-30 09:30:41 +0300

    Deleted security completely, added Liquibase structure, User edited (#1)
    * Deleted security completely, added Liquibase structure, User edited
    
    * empty lines added

--- src/main/java/com/carsharing/model/User.java
+++ src/main/java/com/carsharing/model/User.java
@@ -1,4 +1,36 @@
 package com.carsharing.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    @ToString.Exclude
+    private String password;
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    private Role role;
+
+    public enum Role {
+        MANAGER, CUSTOMER
+    }
 }
